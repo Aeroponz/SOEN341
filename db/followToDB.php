1ae4227039a7da6d56169b788f98e32dacf20c86
Jasen Ratnam <jasenratnam@hotmail.com>
2020-02-09 20:14:10 -0500

    Add files and code to follow a user, uses default user numbers

---
+++ db/followToDB.php
@@ -0,0 +1,24 @@
+<?php 
+require_once('../db/DBConfig.php'); 
+	//Author: Jasen Ratnam 40094237
+	
+	//TODO: Update to session values when available, update to new page after following
+	$u_id = 2; 
+	$redirect_path = '/BLU/db/dbPHPexample.php';
+	
+	// User ID of account you want to follow
+	// get this from user profile page when its done
+	$u_id2 = 3; 
+	
+	//Declare variables
+	$dbconn = null;
+	$sql = null;
+		
+	$sql = "INSERT INTO follow_tbl (u_id, follows) VALUES($u_id, $u_id2)";
+	
+	$dbconn = Database::getConnection();
+	$dbconn->query($sql);
+	$dbconn = null;		
+	
+	header('Location: '.$uri. $redirect_path);
+?>
