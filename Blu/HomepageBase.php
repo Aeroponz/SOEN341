e6ec0bd4c569d1d6dc039cd99728bf0757ec3dd4
Jasen Ratnam <jasenratnam@hotmail.com>
2020-02-12 18:16:27 -0500

    Fix following and posting to use logged in user_ID

--- Blu/HomepageBase.php
+++ Blu/HomepageBase.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+require_once('../db/DBConfig.php'); //Must have at the top of any file that needs db connection.
+require_once('../uploadBlock.php'); //Must have at the top of any page that will be able to post.
+require_once('../followBlock.php'); 
+session_start();
+?>
+
 <html>
 
 	<head>
@@ -9,6 +16,16 @@
 	</head>
 
 	<body>
+		<!-- get current user id -->
+		<?php
+		 if (isset($_SESSION["userID"])) {
+			 $loggenOnUser = $_SESSION["userID"];
+			 echo "Found User: ", $loggenOnUser, "<br />";
+		 } else {
+			 $loggenOnUser = " a public user";
+		 }
+		?>
+		
 		<!--Header Format-->
 		<div id = "container_top">
 			<img class="post" src = "Blu_logo_square.png" alt="Company Logo"
@@ -30,6 +47,21 @@
 		<div class = "main">
 			<!--Post format-->
 			<div id = "container_post">
+				<div id="LoggedInUser" class="fluid ">
+					<!-- output current user id -->
+					Hi.  I'm <?php echo $loggenOnUser; ?> 
+				</div>
+				
+				<!-- inserting post upload block -->
+				<?php
+					echo uploadBlock::insertForm();
+				?>
+				
+				<!-- inserting follow block -->
+				<?php
+					echo followblock::follow();
+				?>
+				
 				<h2>Post Title</h2>
 				<!-- image source taken from url. (not permanent)-->
 				<img class="post" src = "https://i.pinimg.com/736x/36/f7/95/36f795ef5eec0acb7ea035e10102630d.jpg" alt="Test pic of a flower (Identifier)"/>
