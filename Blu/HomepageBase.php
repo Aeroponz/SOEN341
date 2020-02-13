1d05266ff68b1b4fabcc1ff7491c8aff346c8e3b
Jasen Ratnam <jasenratnam@hotmail.com>
2020-02-13 07:26:59 -0500

    Follow and post using real user_id (#82)
    * Fix following and posting to use logged in user_ID
    
    * add list of users and who they are following
    
    * Fixed path

--- Blu/HomepageBase.php
+++ Blu/HomepageBase.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html>
+<?php
+require_once('../db/DBConfig.php'); //Must have at the top of any file that needs db connection.
+require_once('../uploadBlock.php'); //Must have at the top of any page that will be able to post.
+require_once('../followBlock.php'); 
+session_start();
+?>
+
 <html>
 
 	<head>
@@ -9,6 +16,16 @@
 	</head>
 
 	<body>
+		<!-- get current user id -->
+		<?php
+		 if (isset($_SESSION["userID"])) {
+			 $loggenOnUser = $_SESSION["userID"];
+			 echo "Found User: ", $loggenOnUser, "<br />";
+		 } else {
+			 $loggenOnUser = " a public user";
+		 }
+		?>
+		
 		<!--Header Format-->
 		<div id = "container_top">
 			<img class="post" src = "Blu_logo_square.png" alt="Company Logo"
@@ -30,6 +47,43 @@
 		<div class = "main">
 			<!--Post format-->
 			<div id = "container_post">
+				<div id="LoggedInUser" class="fluid ">
+					<!-- output current user id -->
+					Hi.  I'm <?php echo $loggenOnUser; ?> 
+				</div>
+				
+				<!-- inserting post upload block -->
+				<?php
+					echo uploadBlock::insertForm();
+				?>
+				
+				<!-- inserting follow block -->
+				<?php
+					echo followblock::follow();
+				?>
+				
+				<!--fetch account followers -->
+				<?php
+					//connection instance
+					$dbconn = Database::getConnection();
+					
+					$sql = "SELECT u_id, follows FROM follow_tbl";
+					$result = $dbconn->query($sql);
+					
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							echo "User: " . $row["u_id"]. " - Follows: " . $row["follows"]. "</br>";
+						}
+					} else {
+						echo "0 results";
+					}
+					
+					//deallocate memory. 
+					//MUST BE DONE AFTER YOU'RE FINISHED WITH A DB CONNECTION
+					$dbconn = null;
+				?>
+				
 				<h2>Post Title</h2>
 				<!-- image source taken from url. (not permanent)-->
 				<img class="post" src = "https://i.pinimg.com/736x/36/f7/95/36f795ef5eec0acb7ea035e10102630d.jpg" alt="Test pic of a flower (Identifier)"/>
---
+++ Blu/followToDB.php
@@ -0,0 +1,33 @@
+<?php 
+require_once('../db/DBConfig.php'); 
+session_start();
+
+	//Author: Jasen Ratnam 40094237
+		//get current user id
+		 if (isset($_SESSION["userID"])) {
+			 $loggenOnUser = $_SESSION["userID"];
+			 echo "Found User: ", $loggenOnUser, "<br />";
+		 } else {
+			 $loggenOnUser = " a public user";
+		 }
+	
+	$u_id = $loggenOnUser; 
+	
+	$redirect_path = 'HomepageBase.php';
+	
+	// User ID of account you want to follow
+	// get this from user profile page when its done
+	$u_id2 = 3; 
+	
+	//Declare variables
+	$dbconn = null;
+	$sql = null;
+		
+	$sql = "INSERT INTO follow_tbl (u_id, follows) VALUES($u_id, $u_id2)";
+	
+	$dbconn = Database::getConnection();
+	$dbconn->query($sql);
+	$dbconn = null;		
+	
+	header('Location: '.$uri. $redirect_path);
+?>
