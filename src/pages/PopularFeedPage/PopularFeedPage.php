febff6dd699cde0ce1d2259547b44c606734a895
nathanziri <nathanziri@hotmail.com>
2020-02-14 16:45:27 -0500

    Multiple CSS changes user and popular feed added, Removed header and place in it's own folder.

--- src/pages/PopularFeedPage/PopularFeedPage.php
+++ src/pages/PopularFeedPage/PopularFeedPage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width-device-width, initial-scale=1"/>
-    <title>User Feed</title>
+    <title>Popular Feed</title>
     <link rel="stylesheet" href="../css/FeedStyle.css"/>
 </head>
 <body>
