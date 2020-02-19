e429ab52103c8cc92025abdb6556700c549be876
Alya Naseer <AlyaNaseer@users.noreply.github.com>
2020-02-19 12:22:59 -0500

    Set up testing email server, reset password process complete

--- src/pages/ResetPasswordPage/resetPassword.php
+++ src/pages/ResetPasswordPage/resetPassword.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<?php require_once('C:/xampp/htdocs/SOEN341/src/db/DBConfig.php'); ?>
+<?php require_once('../../db/DBConfig.php'); ?>
 <html lang = "en">
 	<head>
 		<meta charset = "utf-8">
-		<title>Home</title>
+		<title>Reset Password</title>
 		<link rel = "stylesheet" type = "text/css" href = "resetPasswordStyle.css"/>
 	</head>
 	<body>
 		<center>
 		<div class = "backgroundColor">
-			<img src = "http://localhost/SOEN341/src/pages/GenericRessources/Blu.png" class = "logoLogin">
+			<img src = "../GenericResources/Blu.png" class = "logoLogin">
 			<br><br><br>
 			<p>Enter below the email address associated to your account</p>
 			<?php session_start();?>
@@ -33,14 +33,13 @@
 			$dbconnection = null;
 			$validEmail = true;
 			$_SESSION['userID'] = "";
+			$_SESSION['email'] = "";
 			while($row = $result->fetch_assoc()){ 	//fetches values of results and stores in array $row 
 				if($row["email"] == $_POST['email']) {
 					$validEmail = true;
 					$_SESSION['userID'] = $row["u_id"];
-					$msg = "test";
-					$email = "alya_naseer@outlook.com";//$row['email'];
-					ini_set();
-					mail($email, "Test", $msg);
+					$_SESSION['email'] = $row["email"];
+					require_once('MailTrap/PhpMailer.php');
 					echo "<script type = \"text/JavaScript\"> 
 					document.getElementById('message').innerHTML = \"An email has been sent with a link to reset your password. <br>Please make sure to check your spam folder\";</script>";
 					break;
