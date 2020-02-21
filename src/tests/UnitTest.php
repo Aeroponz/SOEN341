f6875ab70a6c929a387b2db361590189e88217ea
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-02-21 17:44:52 -0500

    I hate paths :) ...

--- src/tests/UnitTest.php
+++ src/tests/UnitTest.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 //$path = getenv('TRAVIS_BUILD_DIR')."/src/pages/LoginPage/uploadPostToDB.php";
 include 'TestFunction.php';
-//include $path;
+include 'uploadPostToDB.php';
 
 //Login Page
-include "../pages/LoginPage/uploadPostToDB.php";
+//include "../pages/LoginPage/uploadPostToDB.php";
 
 use PHPUnit\Framework\TestCase;
 
