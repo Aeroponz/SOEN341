07397a708a0bae46f9caed688ca55c827bb3c994
Pierre-Alexis Barras <37449731+Pyxsys@users.noreply.github.com>
2020-02-27 14:43:36 -0500

    (#104, 103, #16, #17) refactor of posting code into a functional style + CSS (#114)

--- src/pages/FunctionBlocks/validUpload.js
+++ src/pages/FunctionBlocks/validUpload.js
@@ -13,6 +13,8 @@ function addElement(parentId, elementTag, elementId, class_name, html) {
 	newElement.setAttribute('class', class_name)
     newElement.innerHTML = html;
     p.appendChild(newElement);
+	
+	return true;
 }
 
 // Modifies an element's contents
@@ -34,6 +36,13 @@ function validateUpload(){
 	var validFile = validateFile(); //returns true if file is ok or not there
 	var validText = validateText();	//returns true if there is text that doesnt start with whitespace
 	
+	//removes empty submission error
+	var element = document.getElementById("badPostWarning");
+	if(typeof(element) != 'undefined' && element != null){
+		modifyElement("badPostWarning", "none", "no error");
+	}
+	
+	
 	console.log("Submission Status");
 	console.log(" -file status: " + validFile);
 	console.log(" -text status: " + validText);
@@ -49,19 +58,24 @@ function validateUpload(){
 //returns false on valid file or no file (don't quite understand why js does this)
 function validateFile() {
         var imgfile = document.getElementById('fileinput').files[0];
-		if(imgfile == null){return "isempty";} //if no file -> return "isempty";
+		if(imgfile == null){modifyElement("fl", "block", "Upload an image"); return "isempty";} //if no file -> return "isempty";
 		
 		var regex = new RegExp("(.*?)\.(gif|jpg|jpeg|png|swf|psd|bmp|jpc|jp2|jpx|jb2|swc|iff|wbmp|xbm|ico|webp)$");
 		var passcount = 0;
+		
+		//truncate filename for display
+		var name = (imgfile.name.length < 30) ? imgfile.name : imgfile.name.substring(0,20) + "..." + imgfile.name.substring(imgfile.name.lastIndexOf(".")-3,imgfile.name.length);
+		//modify label to show the selected file
+		modifyElement("fl", "block", name);
 		
 		//Size warning - file must be greater than 12b
 		if(imgfile.size < 12){
 			var element = document.getElementById("sizeWarning");
 			
 			if(typeof(element) != 'undefined' && element != null){
-				modifyElement("sizeWarning", "block", "File is smaller than 12b: \[" + imgfile.name.toLowerCase() + "\] is " + imgfile.size + "b in size.");
+				modifyElement("sizeWarning", "block", "* File is smaller than 12b: \[" + name + "\] is " + imgfile.size + "b in size.");
 			} else{
-				addElement("warnings","p","sizeWarning","upload_warning","File is smaller than 12b: \[" + imgfile.name.toLowerCase() + "\] is " + imgfile.size + "b in size.");
+				addElement("warnings","p","sizeWarning","upload_warning","* File is smaller than 12b: \[" + name + "\] is " + imgfile.size + "b in size.");
 			}
 		}
 		else { 
@@ -74,9 +88,9 @@ function validateFile() {
             var element = document.getElementById("typeWarning");
 			
 			if(typeof(element) != 'undefined' && element != null){
-				modifyElement("typeWarning", "block", "File \[" + imgfile.name.toLowerCase() + "\] is not a valid image type.");
+				modifyElement("typeWarning", "block", "* File \[" + name + "\] is not a valid image type (.jpeg, .png, .gif)");
 			} else{
-				addElement("warnings","p","typeWarning","upload_warning","File \[" + imgfile.name.toLowerCase() + "\] is not a valid image type.");
+				addElement("warnings","p","typeWarning","upload_warning","* File \[" + name + "\] is not a valid image type (.jpeg, .png, .gif)");
 			}
         }
 		else { 
@@ -84,7 +98,6 @@ function validateFile() {
 			passcount += 1;
 		}
 
-		
 		return (passcount == 2);
 }
 
