212c6ea42963698366e4a9a1efdb6416c6dd14a8
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-02-28 12:11:09 -0500

    duplicate include

--- src/pages/FunctionBlocks/checkUsernameAndPassword.php
+++ src/pages/FunctionBlocks/checkUsernameAndPassword.php
@@ -6,7 +6,7 @@
 			return false;
 	}
 	function checkPassword($password){
-		if(preg_match("/^(?=.*\d)(?=.*[A-Za-z])(?=.*[_\W]).{6,}$/",$_POST['password']))
+		if(preg_match("/^(?=.*\d)(?=.*[A-Za-z])(?=.*[_\W]).{6,}$/",$password))
 			return true;
 		else
 			return false;
