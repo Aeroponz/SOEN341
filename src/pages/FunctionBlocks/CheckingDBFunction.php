d11c321b3955829e6cec9d91762c968b944273ba
Alya Naseer <AlyaNaseer@users.noreply.github.com>
2020-02-27 19:05:54 -0500

    Deleted CheckingDBFunction.php

--- src/pages/FunctionBlocks/CheckingDBFunction.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require_once('../../db/DBConfig.php');
-	function checkingDB($query){
-		$dbconnection = Database::getConnection();
-		$result = $dbconnection->query($query);
-		return $result;
-			
-	}
-?>
