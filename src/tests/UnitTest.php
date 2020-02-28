c594904467c1fa6e5c3007227b8d791c9e55b157
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-02-27 21:39:03 -0500

    Adding custom tests. First DB test

--- src/tests/UnitTest.php
+++ src/tests/UnitTest.php
@@ -3,43 +3,31 @@ declare(strict_types=1);
 
 $root = dirname(__FILE__, 3);
 require_once ($root.'/src/db/DBConfig.php');
-
-include 'TestFunction.php';
 include $root.'/src/db/uploadPostToDB.php';
 
 use PHPUnit\Framework\TestCase;
 
 final class UploadPostToDbTest extends TestCase
 {
+    public  function testGenerateString():void
+    {
+        //String Is Random
+        $this -> assertFalse('Plane' == generate_string('Plane'));
+
+        //Default Length
+        $this -> assertTrue(strlen(generate_string('Plane')) == 16);
+
+        //Custom Length
+        $this -> assertTrue(strlen(generate_string('Plane',5)) == 5);
+    }
     public function testCheckForHashtags():void
     {
         $this -> assertEquals('y', check_for_hashtag('#ILOVEPLANES'));
         $this -> assertEquals('n', check_for_hashtag('Plop'));
     }
-
-}
-final class EmailTest extends TestCase
-{
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public  function testFetchUserID():void
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
+        $this -> assertEquals(-1, fetch_user());
     }
 
-    public function testCannotBeCreatedFromInvalidEmailAddress(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString(): void
-    {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
-    }
 }
