74fa04e44970d2e9b1a58bb5bbfa336004cf7e4e
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-02-28 11:58:30 -0500

    Restructured SignUp source code and created unit tests

--- src/pages/SignUpPage/signUp.php
+++ src/pages/SignUpPage/signUp.php
@@ -1,93 +1,118 @@
 <!DOCTYPE html>
-<?php require_once('../../db/DBConfig.php'); 
-	  require('../FunctionBlocks/checkUsernameAndPassword.php');
-?>	
-<html lang = "en">
-	<head>
-		<meta charset = "utf-8">
-		<title>Sign Up</title>
-		<link rel = "stylesheet" type = "text/css" href = "signUpPageStyle.css"/>
-		
-	</head>
-	<body>
-		<center><div class = "backgroundColor">
-			<img src = "../GenericResources/Blu.png" class = "logo">
-		
-			<?php
-				//Outputs a custom message depending if user was redirected to this page.
-				if(isset($_GET["source"])) {
-					if($_GET["source"] == 'post') echo "<p style = \"color:red;\"> You must have an account to create posts.</p>";
-				}				
-			?>
-			
-			<p>Sign up to view photos and videos<br>from your friends and family</p>
-			<?php session_start();?>
-			<form action = "signUp.php" method = "post">
-			
-				<!-- The code: echo htmlspecialchars($_POST['username'], ENT_QUOTES); written below as the value for username was taken from https://www.gamedev.net/forums/topic/564843-how-to-keep-input-fields-after-page-refresh/ -->
-				<input type = "text" name = "username" placeholder = "Username" value = "<?php if (isset($_POST['username'])) {echo htmlspecialchars($_POST['username'], ENT_QUOTES);}?>" class = "creds"/><br><br>
-				<input type = "password" name = "password" placeholder = "Password" class = "creds"/><br><br>
-				<input type = "password" name = "passwordConfirm" placeholder = "Confirm password" class = "creds"/><br><br>
-				<input class = "button" type = "Submit" value = "Sign up" name = "submit"/><br><br>
-				<label id = "password" class = "message"></label><br><br>
-				<div class = "passwordRequirements" align = "left">
-					<label>Your password must include at least:</label>
-					<ul>
-						<li>6 characters</li>
-						<li>One number and one letter</li>
-						<li>One special character</li>
-					</ul>
-				</div><br>
-				<label><b>Have an account? <a class = "login" href = "../LoginPage/LoginPage.php"> Sign in </a></label></b>		
-		</div></center>
+<?php
+$root = dirname(__FILE__, 4);
+require_once($root . '/src/db/DBConfig.php');
+require($root . '/src/pages/FunctionBlocks/checkUsernameAndPassword.php');
+?>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="signUpPageStyle.css"/>
+
+</head>
+<body>
+<center>
+    <div class="backgroundColor">
+        <img src="../GenericResources/Blu.png" class="logo">
+
+        <?php
+        //Outputs a custom message depending if user was redirected to this page.
+        if (isset($_GET["source"])) {
+            if ($_GET["source"] == 'post') echo "<p style = \"color:red;\"> You must have an account to create posts.</p>";
+        }
+        ?>
+
+        <p>Sign up to view photos and videos<br>from your friends and family</p>
+        <?php session_start(); ?>
+        <form action="signUp.php" method="post">
+
+            <!-- The code: echo htmlspecialchars($_POST['username'], ENT_QUOTES); written below as the value for username was taken from https://www.gamedev.net/forums/topic/564843-how-to-keep-input-fields-after-page-refresh/ -->
+            <input type="text" name="username" placeholder="Username" value="<?php if (isset($_POST['username'])) {
+                echo htmlspecialchars($_POST['username'], ENT_QUOTES);
+            } ?>" class="creds"/><br><br>
+            <input type="password" name="password" placeholder="Password" class="creds"/><br><br>
+            <input type="password" name="passwordConfirm" placeholder="Confirm password" class="creds"/><br><br>
+            <input class="button" type="Submit" value="Sign up" name="submit"/><br><br>
+            <label id="password" class="message"></label><br><br>
+            <div class="passwordRequirements" align="left">
+                <label>Your password must include at least:</label>
+                <ul>
+                    <li>6 characters</li>
+                    <li>One number and one letter</li>
+                    <li>One special character</li>
+                </ul>
+            </div>
+            <br>
+            <label><b>Have an account? <a class="login" href="../LoginPage/LoginPage.php"> Sign in </a></label></b>
+    </div>
+</center>
 
 
-		<?php if ($_POST) {
-			if(isset($_POST['submit'])){
-				$result = Database::safeQuery("SELECT u_id, name, pass FROM users");
-				$availableUsername = true;
-				$_SESSION['userID'] = "";
-				if(checkUsername($_POST['username']) && checkPassword($_POST['password'])){
-					while($row = $result->fetch_assoc()){ 	//fetches values of results and stores in array $row 
-						if($row["name"] != $_POST['username'])
-								$availableUsername = true;
-						else {
-							$availableUsername = false;
-							break;
-						}
-					}	
+<?php if ($_POST) {
+if (isset($_POST['submit'])) {
+    $availableUsername = true;
+    $_SESSION['userID'] = "";
+    $wUsername = $_POST['username'];
+    $wPassword = $_POST['password'];
+    $wPasswordConfirm = $_POST['passwordConfirm'];
 
-					if($availableUsername == true && $_POST['password'] == $_POST['passwordConfirm']){
-						$username = $_POST['username'];
-						$password = $_POST['password'];
-						$result2 = Database::safeQuery("SELECT u_id FROM users ORDER BY u_id DESC LIMIT 1");
-						$valueID = $result2->fetch_assoc();
-						$valueID['u_id'] += 1;
-						$userIDValue = $valueID['u_id'];
-						echo $userIDValue;
-						$_SESSION['userID'] = $userIDValue;
-						$result3 = Database::safeQuery("INSERT INTO users(u_id, name, pass) VALUES ('$userIDValue', '$username', '$password')");
-						$result4 = Database::safeQuery("INSERT INTO user_profile(u_id) VALUES ('$userIDValue')");
-						header("Location: ModalPopUp.php");
-					}
-					else if($availableUsername == false)
-						echo "<script type = \"text/JavaScript\">
-								document.getElementById('password').innerHTML = \"Username already exists\";
-								</script>";
-					else
-						echo "<script type = \"text/JavaScript\">
+    $_SESSION['userID'] = SignUp($wUsername, $wPassword, $wPasswordConfirm);
+
+}
+function SignUp($iUsername, $iPassword, $iPassConfirm)
+{
+    if (checkUsername($iUsername) && checkPassword($iPassword)) {
+        $availableUsername = get_Username_Availability($iUsername);
+
+        if ($availableUsername == true) {
+            if ($iPassword == $iPassConfirm) {
+                $wDbQuery = Database::safeQuery("SELECT u_id FROM users ORDER BY u_id DESC LIMIT 1");
+                $valueID = $wDbQuery->fetch_assoc();
+                $valueID['u_id'] += 1;
+                $userIDValue = $valueID['u_id'];
+                echo $userIDValue;
+                Database::safeQuery("INSERT INTO users(u_id, name, pass) VALUES ('$userIDValue', '$iUsername', '$iPassword')");
+                Database::safeQuery("INSERT INTO user_profile(u_id) VALUES ('$userIDValue')");
+                header("Location: ModalPopUp.php");
+                return $userIDValue;
+            }
+            echo "<script type = \"text/JavaScript\">
 								document.getElementById('password').innerHTML = \"Passwords don't match\";
 								</script>";
-				}
-				else
-					echo "<script type = \"text/JavaScript\">
+            return -3;
+        } else {
+            echo "<script type = \"text/JavaScript\">
+								document.getElementById('password').innerHTML = \"Username already exists\";
+								</script>";
+            return -2;
+        }
+    } else {
+        echo "<script type = \"text/JavaScript\">
 							document.getElementById('password').innerHTML = \"Your username/password does not match the required format.\";
 						</script>";
-			}
-		}
-		?>
-	</body>
-	
+        return -1;
+    }
+}
+}
+
+function get_Username_Availability($iUsername)
+{
+    $wDbQuery = Database::safeQuery("SELECT u_id, name, pass FROM users");
+    $wOutput = true;
+    while ($row = $wDbQuery->fetch_assoc()) {    //fetches values of results and stores in array $row
+        if ($row["name"] != $iUsername) {
+        } else {
+            $wOutput = false;
+            break;
+        }
+    }
+    return $wOutput;
+}
+
+?>
+</body>
+
 </html>
 
 
---
+++ src/tests/SignUpTest.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+$root = dirname(__FILE__, 3);
+require_once ($root.'/src/db/DBConfig.php');
+include $root.'/src/pages/FunctionBlocks/checkUsernameAndPassword.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class SignUpTest extends TestCase
+{
+    public  function testCheckUsernameRequirements():void
+    {
+        //Username format is valid (No invalid characters)
+        $this -> assertTrue(checkUsername('Ilikeplanes123'));
+
+        //Username format is not valid (Contains invalid characters)
+        $this -> assertFalse(checkUsername('Il!keplane$123'));
+
+    }
+    public  function testCheckPasswordRequirements():void
+    {
+        //Password format is valid (Contains: 1 uppercase, 1 lowercase, 1 special character, 1 number)
+        $this -> assertTrue(checkPassword('Il!keplane$123'));
+
+        //Password format is not valid (Does not contain a special character)
+        $this -> assertFalse(checkPassword('Ilikeplanes123'));
+
+        //Password format is not valid (Does not contain an uppercase character)
+        $this -> assertFalse(checkPassword('ilikeplanes123'));
+
+        //Password format is not valid (Does not contain a lowercase character)
+        $this -> assertFalse(checkPassword('ILIKEPLANES123'));
+
+        //Password format is not valid (Does not contain a number)
+        $this -> assertFalse(checkPassword('ILIKEPLANES'));
+
+        //Password format is not valid (Too short)
+        $this -> assertFalse(checkPassword('Il!2'));
+
+    }
+    //Summary
+    //This test will test the boundaries of the Signup Page. The user created will be used in the remainder of the tests.
+    public  function testSignUp():void
+    {
+        //Password & Confirm Password Mismatch
+        $this -> assertEquals(-3, SignUp('Aeroponz', 'Il!keplanes123', 'Ilikeplane$123'));
+        //Username Invalid at Signup
+        $this -> assertEquals(-1, SignUp('A$!roponz', 'Il!keplanes123', 'Ilikeplane$123'));
+        //Password Invalid at Signup
+        $this -> assertEquals(-1, SignUp('Aeroponz', '12345', '12345'));
+        //User Successfully Created
+        $this -> assertTrue(SignUp('TestUser', 'Tr4v!sCI', 'Tr4v!sCI') > 0);
+        //Duplicate Username
+        $this -> assertEquals(-2, SignUp('TestUser', 'Trav!sC1', 'Trav!sC1'));
+    }
+
+}
