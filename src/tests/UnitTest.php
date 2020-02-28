2fc2e82a0e2116793b20234e9d92d5081357d30b
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-02-27 20:51:11 -0500

    Easier way

--- src/tests/UnitTest.php
+++ src/tests/UnitTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 //define(__DIR__, getenv('TRAVIS_BUILD_DIR'));
 //require_once (__DIR__.'/src/db/DBConfig.php');
 
-$__DIR__ = dirname(dirname(dirname(__FILE__)));
+$__DIR__ = dirname(__FILE__, 3);
 require_once (__DIR__.'/src/db/DBConfig.php');
 
 include 'TestFunction.php';
