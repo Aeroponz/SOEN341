51150376652f4189a0495c4714a779a529f18916
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-03-28 13:50:08 -0400

    Comment Unit tests

--- src/db/commentToDB.php
+++ src/db/commentToDB.php
@@ -28,7 +28,7 @@ class Comment{
 	}
 
 	function FetchPId() {
-		if(isset($this->mP_id) && $this->mP_id !== ''){
+		if(isset($this->mP_id) && $this->mP_id != ''){
 		  $oP_id = $this->mP_id;
 		  //echo $p_id; //comment out echo when not debugging
 		} else {
@@ -39,24 +39,29 @@ class Comment{
 	
 	function AddCommentToDb(){
 		//Declare variables
-		$wSql = null;
 		$wU_id = $this->FetchUser();
 		$wText = $this->mCommentText;
 		$wP_id = $this->FetchPId();
-		$oCommentType = -1;
-		
-		if($wU_id == -1){$oCommentType = -3;}
-		
-		if($p_id == -1){$oCommentType = -4;}
-		
-		if( isset($wText) & $wText != '' & !ctype_space($wText))
-		{
-			$wSql = "INSERT INTO comments (p_id, u_id, txt_content) VALUES($wP_id, $wU_id, '$wText')";
-			$oCommentType = 1;
-			Database::safeQuery($wSql);
-		}
-		return $oCommentType;
+		return $this->AddComment($wU_id, $wText, $wP_id);
 	}
+
+    function AddComment($iU_id, $iText, $iP_id){
+
+        $wSql = null;
+        $oCommentType = -1;
+
+        if($iU_id == -1){$oCommentType = -3;}
+
+        if($iP_id == -1){$oCommentType = -4;}
+
+        if( isset($iText) & $iText != '' & !ctype_space($iText))
+        {
+            $wSql = "INSERT INTO comments (p_id, u_id, txt_content) VALUES($iP_id, $iU_id, '$iText')";
+            $oCommentType = 1;
+            Database::safeQuery($wSql);
+        }
+        return $oCommentType;
+    }
 	
 	//returns a server path to a page
 	function GetRedirectPath($iValue){
@@ -64,12 +69,11 @@ class Comment{
 		$wP_id = $this->FetchPId();
 		if($wP_id == -1){$iValue = -4;}
 		switch($iValue){
-		/*no user*/		 case(-3): return "../SignUpPage/signUP.php?source=post";
-		/*no p_id*/		 case(-4): return "../viewPostPage/viewPost.php?id= $wP_id&source=noP_id";
-		/*no comment*/   case(-1): return "../viewPostPage/viewPost.php?id= $wP_id&source=noComment";
-		/*comment success*/ default: return "../viewPostPage/viewPost.php?id= $wP_id";
+		/*no user*/		 case(-3): return '../SignUpPage/signUP.php?source=post';
+		/*no p_id*/		 case(-4): return '../viewPostPage/viewPost.php?id= $wP_id&source=noP_id';
+		/*no comment*/   case(-1): return '../viewPostPage/viewPost.php?id= $wP_id&source=noComment';
+		/*comment success*/ default: return '../viewPostPage/viewPost.php?id= $wP_id';
 		}
-		return "../viewPostPage/viewPost.php?id= $wP_id";
 	}
 }
 ?>
--- src/tests/CommentTest.php
+++ src/tests/CommentTest.php
@@ -1,35 +1,73 @@
 <?php
+declare(strict_types=1);
 
-namespace SqlDb;
+namespace UnitTesting\Comment;
 
-use Website\Comment;
+$root = dirname(__FILE__, 3);
+require_once($root . '/src/db/DBConfig.php');
+require($root . '/src/db/commentToDB.php');
+
+use Website;
 use PHPUnit\Framework\TestCase;
 
 final class CommentTest extends TestCase
 {
+    var $mComment;
 
-    public function testAddCommentToDb()
+    function _construct()
     {
-        $this->assertFalse(false);
+        $this->mComment = new Website\Comment();
+        $this->mComment->mCommentText = "Testing the comments!";
     }
-
-    public function testGetRedirectPath()
-    {
-        $this->assertFalse(false);
-    }
-
     public function testFetchPId()
     {
-        $this->assertFalse(false);
+       $this->mComment = new Website\Comment();
+       $this->assertTrue( $this->mComment->FetchPId() == -1);
+
+       $this->mComment->mP_id = 1234;
+       $this->assertTrue( $this->mComment->FetchPId() == 1234);
     }
 
     public function testFetchUser()
     {
-        $this->assertFalse(false);
+        $this->mComment = new Website\Comment();
+        $this->assertTrue( $this->mComment->FetchUser() == -1);
+
+        $this->mComment->mU_id = 1234;
+        $this->assertTrue( $this->mComment->FetchUser() == 1234);;
     }
 
-    public function testWithPost()
+    public function testAddComment()
     {
-        $this->assertFalse(false);
+        $this->mComment = new Website\Comment();
+
+        //All Fields Missing
+        $this->assertTrue($this->mComment->AddComment(-1, "", -1) == -3);
+
+        //User not logged in
+        $this->assertTrue($this->mComment->AddComment(-1, "Test Comment", 123) == -3);
+
+        //Failed to Fetch Post ID
+        $this->assertTrue($this->mComment->AddComment(12, "Test Comment", -1) == -4);
+
+        //Empty Comment
+        $this->assertTrue($this->mComment->AddComment(12, "", 123) == -1);
+
+        //Comment added successfully
+        $this->assertTrue($this->mComment->AddComment(12, "Test Comment", 1234) == 1);
+    }
+
+    public function testGetRedirectPath()
+    {
+        $this->mComment = new Website\Comment();
+
+        //No User ID error code
+        $this->assertEquals($this->mComment->GetRedirectPath(-3), '../SignUpPage/signUP.php?source=post');
+        //No Post ID error code
+        $this->assertEquals($this->mComment->GetRedirectPath(-4), '../viewPostPage/viewPost.php?id= $wP_id&source=noP_id');
+        //No Comment Error code
+        $this->assertEquals($this->mComment->GetRedirectPath(-1), '../viewPostPage/viewPost.php?id= $wP_id&source=noComment');
+        //Default (Comment success)
+        $this->assertEquals($this->mComment->GetRedirectPath(1), '../viewPostPage/viewPost.php?id= $wP_id');
     }
 }
