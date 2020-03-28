c117f533cf5a96a0e5534a85c021c1ab36ec49e7
Jean-Philippe Durand <jp.durand@hotmail.com>
2020-03-28 12:45:30 -0400

    Testing the UnitTest creation feature of IntelliJ

---
+++ src/tests/CommentTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SqlDb;
+
+use Website\Comment;
+
+class CommentTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testAddCommentToDb()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testGetRedirectPath()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testFetchPId()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testFetchUser()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testWithPost()
+    {
+        $this->assertFalse(false);
+    }
+}
